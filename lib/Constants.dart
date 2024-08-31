5866c1ea0e81f74d84e3f6b3ee6c1e5c720298df
Rawanesmail99 <rawan.ismail.abdelwahab@gmail.com>
2024-08-31 17:04:50 +0300

    details screen done

---
+++ lib/Constants.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class Constants{
+  static const String Authorization='Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMzNkODVlMDc3MzM3NjdlOWZhYTIwZGU4YzA4ODg5OSIsIm5iZiI6MTcyNDUzMTgzNi45NzM4NTUsInN1YiI6IjY2Y2EzNGIxNmVmMTYxYmNkNjc0ODMwMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jwvseo9Ay1vU8F79b1Z6f-zrwOWJLiSZcvlYOA2UsTE';
+  static const String baseUrl = 'https://api.themoviedb.org/3/movie/';
+  static Color background = const Color(0xff121312);
+  static Color containersBackground = const Color(0xff282a28);
+}
